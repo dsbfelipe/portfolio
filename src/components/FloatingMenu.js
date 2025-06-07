1e21097da5fdc2404171e03e984a46de46c7e6e7
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2025-06-07 07:20:37 -0300

    Add fonts

--- src/components/FloatingMenu.js
+++ src/components/FloatingMenu.js
@@ -1,11 +1,19 @@
 "use client";
 
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
 export default function FloatingMenu() {
   return (
-    <div className="fixed bottom-16 right-32 space-y-2 text-right flex flex-col gap-2.5 z-50">
+    <div
+      className={`${outfit.className} fixed bottom-16 right-32 space-y-2 text-right flex flex-col gap-2.5 z-50`}
+    >
       <a href="#trabalhos">Trabalhos</a>
       <a href="#sobre">Sobre mim</a>
-      <a href="#sobre">Curriculum</a>
+      <a href="#curriculum">Curriculum</a>
     </div>
   );
 }
