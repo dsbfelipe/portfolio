e879b8c6800b5121de054d64e8945f0fcf6521bc
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2025-06-09 06:19:50 -0300

    feat: add work section

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@
 
 import FloatingMenu from "@/components/FloatingMenu";
 import HeroSection from "@/components/HeroSection";
+import WorksSection from "@/components/WorksSection";
 
 export default function Home() {
   return (
     <>
       <FloatingMenu />
       <HeroSection />
+      <WorksSection />
     </>
   );
 }
