a03ea13dc57c00c22a8d7a7ef802afb72ef5aa13
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2025-06-15 20:16:10 -0300

    refactor: hide floating menu (for now)

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ import WorksSection from "@/components/WorksSection";
 export default function Home() {
   return (
     <main>
-      <FloatingMenu />
+      {/* <FloatingMenu /> */}
       <HeroSection />
       <WorksSection />
       <Footer />
